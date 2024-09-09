b89b9a542dbc65c655526ec84aed6dcf0a7201b2
yansuogaming <80459529+yansuogaming@users.noreply.github.com>
2024-09-09 11:34:19 +0700

    Update Code

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Link CSS  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/css/bootstrap-select.min.css">
+
+    <link rel="stylesheet" href="">
+
+</head>
+
+<body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
+    <?php include("./layout/header.php"); ?>
+    <?php include("./layout/home.php"); ?>
+    <?php include("./layout/footer.php"); ?>
+
+    
+
+
+
+</body>
+
+</html>
---
+++ layout/header.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="py-2 bg-light border-bottom">
+        <div class="container d-flex flex-wrap">
+            <ul class="nav ms-auto">
+                <li class="nav-item"><a href="tel:+842438293838" class="nav-link link-dark px-2"><i
+                            class="fa-solid fa-phone me-1"></i>+84 243 829 3838</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2"><i
+                            class="fa-regular fa-envelope me-1"></i>info@vietiso.com</a></li>
+                <select class="selectpicker" data-width="fit" data-live-search="true">
+                    <option data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
+                    <option data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
+                </select>
+            </ul>
+        </div>
+    </nav>
+
+</header>
+
